886d44956c4725885baaabf3ac5adcfa36a31894
Oonryo <oonryo@gmail.com>
2023-05-20 18:30:15 +0300

    done2.0

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,37 +1,50 @@
-const slider = document.querySelectorAll(".slider__item");
-const arrowPrev = document.querySelector(".slider__arrow_prev");
-const arrowNext = document.querySelector(".slider__arrow_next");
-const dots = document.querySelectorAll(".slider__dot");
+const sliders = Array.from(document.querySelectorAll('.slider__item'));
+const prev = document.querySelector('.slider__arrow_prev');
+const next = document.querySelector('.slider__arrow_next');
+const dots = Array.from(document.querySelectorAll('.slider__dot'));
 
-let sliderActive = 0;
+dots[0].classList.add('slider__dot_active');
 
 
-arrowPrev.onclick = () => {
-	if (sliderActive - 1 < 0) {
-		sliderActive = slider.length - 1;
-	} else {
-		sliderActive -= 1;
+function deactivateClass() {
+	let index = sliders.findIndex(e => e === (sliders.find(item => item.classList.contains('slider__item_active'))));
+	sliders[index].classList.remove('slider__item_active');
+	dots[index].classList.remove('slider__dot_active');
+
+	return index;
+}
+
+function activateClass(index) {
+	sliders[index].classList.add('slider__item_active');
+	dots[index].classList.add('slider__dot_active');
+}
+
+function previouClick() {
+	index = deactivateClass();
+	if (index === 0) {
+		index = sliders.length;
 	}
-	changeSlide(sliderActive);
+	index--;
+	activateClass(index);
 }
 
-arrowNext.onclick = () => {
-	if (sliderActive + 1 === slider.length) {
-		sliderActive = 0;
-	} else {
-		sliderActive += 1;
+function nextClick() {
+	index = deactivateClass();
+	if (index === sliders.length - 1) {
+		index = -1;
 	}
-	changeSlide(sliderActive);
+	index++;
+	activateClass(index);
 }
 
-dots[sliderActive].classList.add('slider__dot_active');
-[...dots].forEach((item, i) => item.onclick = () => {
-	changeSlide(i);
-});
+prev.onclick = previouClick;
 
-function changeSlide(i) {
-	[...slider].forEach((element) => element.classList.remove('slider__item_active'));
-	[...dots].forEach((item) => item.classList.remove('slider__dot_active'));
-	slider[i].classList.add('slider__item_active');
-	dots[i].classList.add('slider__dot_active');
-}
+next.onclick = nextClick;
+
+dots.forEach(function (dot) {
+	dot.addEventListener('click', () => {
+		let index = dots.indexOf(dot);
+		deactivateClass();
+		activateClass(index);
+	});
+})
